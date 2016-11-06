61366e10373371f9607ccc978272bea0d5c7b852
Alex Smith <alexsmith1@me.com>
2016-11-06 11:19:23 -0500

    finalizing code to submit

--- swal/linear_classifiers.py
+++ swal/linear_classifiers.py
@@ -6,42 +6,55 @@ from sklearn.model_selection import cross_val_score, train_test_split, KFold
 
 def main():
 
-	logreg = linear_model.LogisticRegression(C=1e5)
+	#define the linear classifiers
+	logreg = linear_model.LogisticRegression(C=1e5,n_jobs=-1,solver='sag')
 	lin_svc = svm.SVC(kernel='linear')
 	rbf_svc = svm.SVC(kernel='rbf', gamma=0.7)
 	poly_svc = svm.SVC(kernel='poly', degree=3)
 
+	#define the different options for testing
 	feature_types = ['sift','surf']
 	k_vals = [20,50]
 	classifiers = [logreg,rbf_svc,poly_svc,lin_svc]
 	cNames = ["Logistic Regression", "Rbf SVM", "Polynomial SVM", "Linear SVM"]
 
-	data = np.fromfile('data/train_x.bin', dtype='uint8')
-	data = data.reshape((100000,60,60))
-	labels = open_csv("data/train_y.csv")
+	try:
+		#open and split data into test and train sets
+		data = np.fromfile("train_x.bin", dtype='uint8')
+		data = data.reshape((100000,60,60))
+		labels = open_csv("train_y.csv")
 
-	data_train = data[0:70000]
-	data_test = data[70000:]
-	Y_train = labels[0:70000]
-	Y_test = labels[70000:]
+		data_train = data[0:70000]
+		data_test = data[70000:]
+		Y_train = labels[0:70000]
+		Y_test = labels[70000:]
 
-	for feat_to_test in feature_types:
-		features_train = feature_extract(data_train,feat_to_test) #extract features for the train set
-		features_test = feature_extract(data_test,feat_to_test) #extract features for the test set
-		vocab_train = create_vocab(features_train,128) #create vocabulary of features for clustering
+		print "WARNING: This will take a while. Expect 4 or more hours."
 
-		for k in k_vals:
-			clusters = sklearn.cluster.MiniBatchKMeans(k)
-			clusters.fit(vocab_train)
+		for feat_to_test in feature_types:
+			#extract features for the train and test set
+			features_train = feature_extract(data_train,feat_to_test)
+			features_test = feature_extract(data_test,feat_to_test)
+			vocab_train = create_vocab(features_train,128)
 
-			X_train = make_vectors(features_train,clusters,k)
-			X_test = make_vectors(features_test,clusters,k)
+			for k in k_vals:
+				#cluster into different k values
+				clusters = sklearn.cluster.MiniBatchKMeans(k)
+				clusters.fit(vocab_train)
 
-			for i, classifier in enumerate(classifiers):
-				classifier.fit(X_train,Y_train)
-				score = 0.8267#classifier.score(X_test,Y_test)
-				print cNames[i] + ' with ' + feat_to_test + ' features, clustered into ' + k + ' groups gives ' + score + " accuracy on 30% of the training data "
+				X_train = make_vectors(features_train,clusters,k)
+				X_test = make_vectors(features_test,clusters,k)
 
+				for i, classifier in enumerate(classifiers):
+					#fit for each k and feature type onto each model
+					classifier.fit(X_train,Y_train)
+					score = classifier.score(X_test,Y_test)
+					print cNames[i] + ' with ' + feat_to_test + ' features, clustered into ' + str(k) + ' groups gives ' + str(score) + " accuracy on 30% of the training data "
+				print
+			print
+
+	except IOError as e:
+		print "Files must be in the same folder as this script."
 
 def feature_extract(data,type,display=False):
 	if type == 'surf':
@@ -104,3 +117,6 @@ def show_img(img,caption):
 	cv2.imshow(caption,img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	main()
